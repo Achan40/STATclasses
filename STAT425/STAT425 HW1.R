c1a25b2358d97de1a37c0a8b58494ba876d15a17
Aaron C <achan40@illinois.edu>
2020-02-05 13:03:27 -0600

    updated homework 1

--- STAT425/STAT425 HW1.R
+++ STAT425/STAT425 HW1.R
@@ -2,18 +2,15 @@
 library(tidyverse)
 library(faraway)
 data("prostate")
-head(prostate)
+help("prostate")
 
-#numerical (explain by hand)
+#numerical
 summary(prostate)
-#graphical (explain by hand)
+#graphical
 pairs(prostate)
 
-#problem 2 (link to proof)
-#https://math.stackexchange.com/questions/129909/correlation-coefficient-and-determination-coefficient/1799567
-
 #problem 3
-invoice = read.delim(file.choose())#manually choose the txt file w data
+invoice = read.delim("invoices.txt")
 slr_invoice = lm(Time ~ Invoices,data = invoice)
 #3a
 plot(invoice$Invoices,invoice$Time,xlab = "Invoices",ylab = "Time")
@@ -21,17 +18,22 @@ abline(slr_invoice)
 #3b
 confint(slr_invoice,level = .95)
 #3c
+summary(slr_invoice)
+t = ((0.0112916-.01)/0.0008184)
+2*pt(-abs(t),df=28)
+#DNR null at alpha=.05
 #3d
 predict_data = data.frame(Invoices = 130)
 predict(slr_invoice,predict_data,interval = "prediction",level = .95)
 
 #problem 5
-indicators = read.delim(file.choose())
+indicators = read.delim("indicators.txt")
 slr_indicators = lm(PriceChange ~ LoanPaymentsOverdue, data = indicators)
 #5a
-summary(slr_indicators)
+summary(slr_indicators)$r.squared
+summary(slr_indicators)$adj.r.squared
 #5b
-confint(slr_indicators)
+confint(slr_indicators,level = .95)
 #5c
 predict_data2 = data.frame(LoanPaymentsOverdue = 4)
 predict(slr_indicators,predict_data2,interval = "confidence",level = .95)
