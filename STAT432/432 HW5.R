dde2616b43f093a77b14839fd44264472b27c555
Aaron C <achan40@illinois.edu>
2020-02-19 14:42:32 -0600

    Finished a few more questions for q5

--- STAT432/432 HW5.R
+++ STAT432/432 HW5.R
@@ -16,3 +16,20 @@ B/C
 .12/(.15+.05+.1+.12)
 #5.9
 .09/(.12+.01+.09)
+
+#5.11
+prior = c(.09,.64,.27)
+X=3
+dnorm(X,8,1.5)*prior[1]
+dnorm(X,8.3,1.5)*prior[2]
+dnorm(X,8.7,1.5)*prior[3]
+#choose largest?
+
+#5.12
+prior = c(.21,.5,.29)
+X=4
+dpois(X,4.43)*prior[1]
+dpois(X,4.8)*prior[2]
+dpois(X,8.6)*prior[3]
+
+#5.14
