896af6d6f6208511f4faeccba535cd50400b40f9
Aaron <51930273+Achan40@users.noreply.github.com>
2020-02-21 00:28:22 -0600

    finished STAT 425 HW2

--- Aaron/STAT425/STAT425 HW2.R
+++ Aaron/STAT425/STAT425 HW2.R
@@ -5,11 +5,32 @@ head(sat)
 
 #a
 sat_model1 = lm(total ~ expend+ratio+salary,data = sat)
+mod1sum = summary(sat_model1)
+
+#H0: Bsalary = 0
+tStat= (mod1sum$coef[4,1]-0)/mod1sum$coef[4,2]#Bsalaryhat and its sterror
+pVal= dt(tStat,df=length(sat)-1)
+pVal
+#Do not reject H0 at alpha=0.05
+
+#H0: Bsalary=Bratio=Bexpend=0
 summary(sat_model1)
+#p-value = 0.01209 since p-value of 0.01209 is less than alpha=0.05, we can reject H0, and we can say that there is at least one coefficient that is different from 0
+#At least one of these predictors has an effect on the response because H0 was rejected due to the p-value being less than 0.05
 
 #b
 sat_model2 = lm(total ~ expend+ratio+salary+takers,data = sat)
-summary(sat_model2)
+mod2sum = summary(sat_model2)
+#H0: Btakers = 0
+tStat2= (mod2sum$coef[5,1]-0)/mod2sum$coef[5,2]#Btakershat and its sterror
+pVal2= dt(tStat2,df=length(sat)-1)
+pVal2
+#Reject H0 at alpha=0.05
+#The new model has a p-value that is essentially 0 even with an extra variable, meaning that it is better than the first model
+#the Ftest is the Ttest squared
+tStat2^2
+anova(sat_model2)$"F value"[4]#F value for takers
+#The F value for takers is 157.7379, which is essentially our previous t-statistic of squared: (-12.557)^2=157.7379
 
 #Problem 2
 data("prostate")
@@ -38,6 +59,7 @@ ggplot(data=myCR, aes(x=age, y=lbph, colour=level)) +
   geom_path(aes(linetype=level), size=1.5) + 
   geom_point(x=coef(pro_model1)[4], y=coef(pro_model1)[5], shape=3, size=3, colour='red') + 
   geom_point(x=0, y=0, shape=1, size=3, colour='red')
+#This is an F test, since the origin is withing the region, we do not reject H0: age = lbph we cannot say there is a difference in means
 
 #d
 #permutation test corresponding to the t-test for age
@@ -58,11 +80,30 @@ data("punting")
 head(punting)
 punt_model1 = lm(Distance ~ RStr+LStr+RFlex+LFlex,data = punting)
 summary(punt_model1)
+qt(.90,df=length(punting)-1,lower.tail=TRUE)
+qt(.10,df=length(punting)-1,lower.tail=TRUE)
+#at the 5% level, only RFlex is a significant predictor
 
 #b
 qf(.95,4,8)
+summary(punt_model1)$fstat
+#collectively, these variable are significant since Fstatistic for the model is further than Fvalue at the 5% level
 
 #c
+#H0: RStr = LStr
+RStrmu= mean(punting$RStr)
+RStrsd= sd(punting$RStr)
+LStrmu= mean(punting$LStr)
+LStrsd= sd(punting$LStr)
+n1= length(punting$RStr)
+n2= length(punting$LStr)
+
+sPooledsq= (((n1-1)*RStrsd^2)+((n2-1)*LStrsd^2))/(n1+n2-2)
+tvalPool= (RStrmu-LStrmu)/(sqrt(sPooledsq*((1/n1)+(1/n2))))
+tvalPool
+pvalPool= dt(tvalPool, df=n1+n2-2)
+pvalPool
+#Given a pval of 0.3589, we can't reject H0 at the 5% level, meaning that right and left strength could have the same effect.
 
 #d
 CR95 = ellipse(punt_model1,c(2,3))#create confidence region for age&lbph
@@ -75,9 +116,11 @@ ggplot(data=myCR, aes(x=RStr, y=LStr, colour=level)) +
   geom_path(aes(linetype=level), size=1.5) + 
   geom_point(x=coef(punt_model1)[4], y=coef(punt_model1)[5], shape=3, size=3, colour='red') + 
   geom_point(x=0, y=0, shape=1, size=3, colour='red')
+#The confidence region tests H0: RStr = LStr, since the origin is within the region, we can see that we can't reject H0
 
 #e
 punt_model2 = lm(Distance ~ I(RStr+LStr)+RFlex+LFlex,data = punting)
+summary(punt_model2)
 
 #Problem 5
 
@@ -89,6 +132,7 @@ predict(pro_model3,predictdata,interval = "confidence",level = .95)
 #b
 predictdata2 = data.frame(lcavol=1.44692,lweight=3.62301,age=20,lbph=.3001,svi=0,lcp=-.79851,gleason=7,pgg45=15)
 predict(pro_model3,predictdata2,interval = "confidence",level = .95)
+#Wider CI since maybe not much data is known for younger males w prostate cancer as it does not happen to younger people often
 
 #c
 summary(pro_model3)#remove age,lbph,lcp,gleason,pgg45
@@ -96,3 +140,4 @@ pro_model4 = lm(lpsa ~ lcavol+lweight+svi,data = prostate)
 
 predictdata = data.frame(lcavol=1.44692,lweight=3.62301,svi=0)
 predict(pro_model4,predictdata,interval = "confidence",level = .95)
+#The confidence interval is tigher now, I would prefer this one as it does not depend on age (a predictor that we may not have a lot of data for)
