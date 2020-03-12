0a9677b869b542bb3d047d804290b104f3fe124a
Aaron C <achan40@illinois.edu>
2020-03-12 15:56:58 -0500

    only 1b left for HW3

--- Aaron/STAT426/STAT 426 HW3.R
+++ Aaron/STAT426/STAT 426 HW3.R
@@ -8,6 +8,9 @@ horseshoe.ols = lm(y ~ weight, data = horseshoe)
 summary(horseshoe.ols)
 
 predict(horseshoe.ols, data.frame(weight = 5200), type = "response")
+#For crab with 0 weight the estimated probability of crab with a satellite is -.01449, does not make too much sense though since each crab must have a weight
+#As weight increases by 1kg, the estimated probability of a creab having a satellite increases by .0003227
+#For the predicted probability of a crab having a satellite with weight = 5200, a probability of 1.533186 does not make too much sense
 
 #b
 horseshoe.ML = 
@@ -43,7 +46,32 @@ ratios.dat = data.frame(
 )
 ratios.dat = cbind(ratios.dat,Ratio)
 ratios.dat
+#A higer ratio means that death rate is higher for smokers than for non-smokers.
+#A Ratio < 1 means that the death rate is lower for smokers than non-smokers.
+#When age increases, the Ratios decrease meaning that deaths because of smoking decrease as age increases.
 
 #b
 mainEfftects = glm(Deaths ~ Age + Smoke, family = poisson, offset = log(PersonYears), data = Smoke.dat)
-summary(mainEfftects)
+mainEfftects
+#Our ratio of rates table contradicts the main effects models findings, since the effects of smoking on death is less strong as age increases.
+#This shows that the main effects model assumes a constant ratio of nonsmokers to smokers coronary death rates over age.
+#There for the main effects model is the the best to model the smoking-death relationship.
+
+#c
+#In a, we can see a linear relationship between death rate and age, so we can have them interact.
+#The model: log(u/t) = a + B1age + B2smoking + B3(smoking*age)
+
+#Assign Scores
+Smoke.datNew = data.frame(
+  AgeScore = c(rep(1,2),rep(2,2),rep(3,2),rep(4,2),rep(5,2)),
+  PersonYears = c(18793,52407,10673,43248,5710,28612,2585,12663,1462,5317),
+  Smoke = c(rep(c("no","yes"),5)),
+  Deaths = c(2,32,12,104,28,206,28,186,31,102)
+)
+
+SmokeQuant = glm(Deaths ~ AgeScore*Smoke, family = poisson, offset = log(PersonYears), data = Smoke.datNew)
+SmokeQuant
+
+#The main effects model has a lower deviance and AIC than the quant model.
+#The quat model might not bet the best since the log of the rate does not change with age linearly.
+#Adding an age^2 might better fit the model since to make age no longer a linear relationship.
