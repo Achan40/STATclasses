b28a5910987c3d43173c9f3a4b8756407e73c274
Aaron <51930273+Achan40@users.noreply.github.com>
2020-03-28 11:45:27 -0500

    found the correct model for question 2

--- Aaron/STAT426/STAT426 HW4.R
+++ Aaron/STAT426/STAT426 HW4.R
@@ -1,20 +1,17 @@
 setwd("/Users/chanm/Desktop/STATclasses/Aaron/STAT426")
 CancerR = data.frame(read.csv(file = 'cancer-remission.csv',header = T))
 #Problem 2
-#add a binary remission column 1=remission 0=no remission
-CancerR$Remission = c(rep(0,5),1,1,1,0,1,1,0,1,1)
-mod1 = glm(Remission ~ LI/100,data = CancerR, family = binomial)
+#add a counts for number of Remission and number not in Remission
+CancerR$Remission_Y = CancerR$NR
+CancerR$Remission_N = CancerR$NC - CancerR$NR 
+mod1 = glm(cbind(Remission_Y,Remission_N) ~ LI,data = CancerR, family = binomial)
 summary(mod1)
 #a
 predict(mod1, data.frame(LI = 8), type = "response")
 
 #b
-coef(mod1)
--coef(mod1)[1]/coef(mod1)[2]
+coef(mod2)
+-coef(mod2)[1]/coef(mod2)[2]
 
 #c
 
-#################################################
-testing = data.frame(lapply(CancerR, rep, CancerR$NC))
-mod2 = glm(Remission ~ LI,data = testing, family = binomial)
-summary(mod2)
