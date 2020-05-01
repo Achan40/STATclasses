de1abe5d6476d9540b75751c109c32573dbb8058
Aaron <51930273+Achan40@users.noreply.github.com>
2020-05-01 11:53:37 -0500

    Finished HW7

--- Aaron/STAT426/STAT426 HW7.R
+++ Aaron/STAT426/STAT426 HW7.R
@@ -29,4 +29,20 @@ summary(mod)
 setwd("/Users/chanm/Desktop")
 auto = data.frame(read.csv(file = "auto.csv", header = T))
 
-mod2 = vglm(cbind(X1,X2,X3,X4,X5) ~ G + L + S + L * S, family = cumulative(parallel = TRUE), data = auto)
+mod2 = vglm(cbind(X1,X2,X3,X4,X5) ~ I(G == "Female") + I(L == "Rural") * I(S == "No"), family = cumulative(parallel = TRUE), data = auto)
+summary(mod2)
+
+#a
+exp(coef(mod2)[1:4])/(1+exp(coef(mod2)[1:4]))
+#For a male who wears his seatbelt and has an accident in an urban area, he has about a 96.47% chance of not being injured.
+
+#b
+exp(coef(mod2)[5]+c(-1,1)*1.96*(0.02721))
+#As we take samples n->infinity 95% of those samples that the odds of a female being in category j or less, where j=1,2,3,4, are between 0.54904 and 0.6108396 the times for males.
+
+#c
+exp(coef(mod2)[7]+coef(mod2)[8])
+#The estimated cumulative odds ratio between the response and seat-belt use for rural locations is approx 0.41289.
+exp(coef(mod2)[7])
+#The estimated cumulative odds ratio between the response and seat-belt use for urban locations is approx 0.4676.
+#The effect of seatbelt use on the odds of injuries is more noticeable in rural areas than urban.
