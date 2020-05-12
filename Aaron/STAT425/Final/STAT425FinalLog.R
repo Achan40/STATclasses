ee9ba3aa802311c2f6988804c42d395e05c1bda2
Aaron <51930273+Achan40@users.noreply.github.com>
2020-05-11 20:06:10 -0500

    done with coding part of the project

--- Aaron/STAT425/Final/STAT425FinalLog.R
+++ Aaron/STAT425/Final/STAT425FinalLog.R
@@ -10,64 +10,56 @@ Resort = Resort[Resort$hotel == "Resort Hotel",]
 
 #Exploratory Analysis
 #Remove unneccsary time variables as well as uneccesarry variables
-Resort = subset(Resort, select = -c(arrival_date_year, arrival_date_week_number, arrival_date_day_of_month, hotel, reserved_room_type, meal, market_segment, customer_type))
+Resort = subset(Resort, select = -c(arrival_date_year, arrival_date_week_number, arrival_date_day_of_month, hotel, meal, market_segment, customer_type))
 Resort$is_canceled = as.factor(Resort$is_canceled)
 
 #Testing Non-linearity
-#Figure 1
 par(mfrow = c(2,2))
-plot(Resort$lead_time,Resort$adr, ylab = "adr", xlab = "lead_time")
-plot(Resort$stays_in_weekend_nights, Resort$adr, ylab = "adr", xlab = "stays_in_weekend_nights")
-plot(Resort$stays_in_week_nights, Resort$adr, ylab = "adr", xlab = "stays_in_week_nights")
-plot(Resort$total_of_special_requests, Resort$adr, ylab = "adr", xlab = "total_of_special_requests")
-title("Figure 1", line = -1, outer = T)
+#Create time variable
+
 
 #Model creation
-#Simple model with interation term (base model)
-mod1 = glm(adr ~ ., data = Resort)
+#Simple model 
+mod1 = glm(is_canceled ~ ., data = Resort, family = binomial)
 summary(mod1)
+par(mfrow = c(2,2))
 plot(mod1)
+title("Figure 2", line = -1, outer = T)
+
 #Residuals vs Fitted shows that we can assume a linear relationship between explanatory variables and the response
 #Normal QQ plot shows our residuals are not exactly normal, perform shapiro test to be sure
-shapiro.test(residuals(mod1))
 #The result of sharpio-wilks testing H0 = data is normally distributed vs Ha = data is not normally distributed, shows we reject H0, data is not normally distributed. May have to perform transformation of our data
 #Scale-Location plot shows variance is not homogenous, may need to perform a transformation
 #Residuals vs Leverage plot shows that we have some high leverage points, some exceed 3 standard deviations, we may have to remove them top get a better model, could be a result of non-normality in our data
-#influential points
+#Influential points
+par(mfrow = c(1,1))
 plot(mod1,4)
-#collinearity
-vif(mod1)
+title("Figure 2.1", line = -1, outer = T)
+#multicollinearity
+car::vif(mod1)
 
 #Model using backward regression w/ AIC and with interaction terms
-mod2 = glm(adr ~ . - children - babies + adults:children + adults:babies,data = Resort)
+mod2 = glm(is_canceled ~ . - children - babies + adults:children + adults:babies,data = Resort, family = binomial)
 mod.back = step(mod2, direction = "backward")
 summary(mod.back)
+par(mfrow = c(2,2))
 plot(mod.back)
+title("Figure 3", line = -1, outer = T)
+
 #Residuals vs Fitted shows that we can assume a linear relationship between explanatory variables and the response
 #Normal QQ plot shows our residuals are not exactly normal, perform shapiro test to be sure
-shapiro.test(residuals(mod.back))
 #The result of sharpio-wilks testing H0 = data is normally distributed vs Ha = data is not normally distributed, shows we reject H0, data is not normally distributed. May have to perform transformation of our data
 #Scale-Location plot shows variance is not homogenous, may need to perform a transformation
 #Residuals vs Leverage plot shows that we have some high leverage points, some exceed 3 standard deviations, we may have to remove them top get a better model, could be a result of non-normality in our data
-#influential points
+#Influential points
+par(mfrow = c(1,1))
 plot(mod.back,4)
-#collinearity
-vif(mod2)
-
-#Train Test Split for Prediction
-set.seed(10)
-trainInt = sample(1:nrow(Resort), 400)
-train = Resort[trainInt,]
-test = Resort[-trainInt,]
-
-#Calculate MSE for the accuracy of the model
-pred = predict(mod2, test)
-test.err = with(test, mean((adr - pred)^2))
-test.err
+title("Figure 3.1", line = -1, outer = T)
+#Multicollinearity
+car::vif(mod2)
 
 #Adjusted R-square to determine significant variables
-
-r2mod = regsubsets(adr ~ . - children - babies + adults:children + adults:babies,data = Resort)
+r2mod = regsubsets(is_canceled ~ . - children - babies + adults:children + adults:babies,data = Resort)
 r2s = summary(r2mod)
 r2s$which
 msize = 2:9
@@ -82,31 +74,39 @@ selectVar = colnames(r2s$which)[r2s$which[which.min(r2s$cp),]]
 selectVar = selectVar[-1]
 selectVar
 
-#Randomforest 
+#Train Test Split for Prediction
+set.seed(100)
+trainInt = sample(1:nrow(Resort), .6*nrow(Resort))
+train = Resort[trainInt,]
+test = Resort[-trainInt,]
 
-#Split into train and validation data 
-trainInt = sample(1:nrow(Resort), 400)
+#Calculate accuracy of the model
+prob = predict(mod2, test, type = "response")
+predClass = ifelse(prob > .5, 1, 0)
+mean(predClass == test$is_canceled)
+
+#Randomforest 
+#Train Test split
+trainInt = sample(1:nrow(Resort), .6*nrow(Resort))
 train = Resort[trainInt,]
 test = Resort[-trainInt,]
 
 #Starting Model
-mlMod = randomForest(adr ~ ., data = Resort, subset = trainInt)
+mlMod = randomForest(is_canceled ~ ., data = Resort, subset = trainInt)
 mlMod
-#plotting error vs number of trees
-plot(mlMod)
 
-test.err = double(10)
-
+#Initialize list
+test.acc = double(10)
 #mtry is the number of variables randomly chosen at each split
 for(mtry in 1:10) 
 {
-  rf = randomForest(adr ~ . , data = Resort, subset = trainInt, mtry = mtry,ntree = 400) 
-  pred = predict(rf,test) #Predictions on Test Set for each Tree
-  test.err[mtry] = with(test, mean((adr - pred)^2)) #Mean Squared Test Error
+  rf = randomForest(is_canceled ~ . , data = Resort, subset = trainInt, mtry = mtry, ntree = 400) 
+  prob.rf = predict(rf,test,type = "response") #Predictions on test Set for each tree
+  test.acc[mtry] = with(test, mean(is_canceled == prob.rf)) #Mean Squared Test Error
 }
-test.err
-#minmize MSE at mtry = 5
-min(test.err)
+test.acc
+#maximum test accuracy at mtry = 2 
+max(test.acc)
 
-mlMod2 = randomForest(adr ~ . - children - babies + adults:children + adults:babies, data = Resort, subset = trainInt, mtry = 5,ntree = 400) 
+mlMod2 = randomForest(is_canceled ~ . - children - babies + adults:children + adults:babies, data = Resort, subset = trainInt, mtry = 2,ntree = 400) 
 mlMod2
--- Aaron/STAT425/Final/Scrapped/STAT425FinalMult.R
+++ Aaron/STAT425/Final/Scrapped/STAT425FinalMult.R
@@ -24,9 +24,10 @@ title("Figure 1", line = -1, outer = T)
 
 #Model creation
 #Simple model with interation term (base model)
-mod1 = glm(is_canceled ~ ., data = Resort, family = binomial)
+mod1 = glm(adr ~ ., data = Resort)
 summary(mod1)
 plot(mod1)
+title("Figure 2", line = -1, outer = T)
 #Residuals vs Fitted shows that we can assume a linear relationship between explanatory variables and the response
 #Normal QQ plot shows our residuals are not exactly normal, perform shapiro test to be sure
 shapiro.test(residuals(mod1))
@@ -34,15 +35,19 @@ shapiro.test(residuals(mod1))
 #Scale-Location plot shows variance is not homogenous, may need to perform a transformation
 #Residuals vs Leverage plot shows that we have some high leverage points, some exceed 3 standard deviations, we may have to remove them top get a better model, could be a result of non-normality in our data
 #influential points
+par(mfrow = c(1,1))
 plot(mod1,4)
+title("Figure 2.1", line = -1, outer = T)
 #collinearity
 vif(mod1)
 
 #Model using backward regression w/ AIC and with interaction terms
-mod2 = glm(is_canceled ~ . - children - babies + adults:children + adults:babies,data = Resort, family = binomial)
+mod2 = glm(adr ~ . - children - babies + adults:children + adults:babies,data = Resort)
 mod.back = step(mod2, direction = "backward")
 summary(mod.back)
+par(mfrow = c(2,2))
 plot(mod.back)
+title("Figure 3", line = -1, outer = T)
 #Residuals vs Fitted shows that we can assume a linear relationship between explanatory variables and the response
 #Normal QQ plot shows our residuals are not exactly normal, perform shapiro test to be sure
 shapiro.test(residuals(mod.back))
@@ -50,24 +55,14 @@ shapiro.test(residuals(mod.back))
 #Scale-Location plot shows variance is not homogenous, may need to perform a transformation
 #Residuals vs Leverage plot shows that we have some high leverage points, some exceed 3 standard deviations, we may have to remove them top get a better model, could be a result of non-normality in our data
 #influential points
+par(mfrow = c(1,1))
 plot(mod.back,4)
+title("Figure 3.1", line = -1, outer = T)
 #collinearity
 vif(mod2)
 
-#Train Test Split for Prediction
-set.seed(100)
-trainInt = sample(1:nrow(Resort), .6*nrow(Resort))
-train = Resort[trainInt,]
-test = Resort[-trainInt,]
-
-#Calculate accuracy of the model
-prob = predict(mod2, test, type = "response")
-predClass = ifelse(prob > .5, 1, 0)
-mean(predClass == test$is_canceled)
-
 #Adjusted R-square to determine significant variables
-
-r2mod = regsubsets(is_canceled ~ . - children - babies + adults:children + adults:babies,data = Resort)
+r2mod = regsubsets(adr ~ . - children - babies + adults:children + adults:babies,data = Resort)
 r2s = summary(r2mod)
 r2s$which
 msize = 2:9
@@ -82,28 +77,42 @@ selectVar = colnames(r2s$which)[r2s$which[which.min(r2s$cp),]]
 selectVar = selectVar[-1]
 selectVar
 
+#Train Test Split for Prediction 60/40
+set.seed(10)
+trainInt = sample(1:nrow(Resort), .6*nrow(Resort))
+train = Resort[trainInt,]
+test = Resort[-trainInt,]
+
+#Calculate MSE for the accuracy of the model
+pred = predict(mod2, test)
+test.err = with(test, mean((adr - pred)^2))
+test.err
+
 #Randomforest 
 
-#Split into train and validation data (70/30)
+#Split into train and validation data 60/40
 trainInt = sample(1:nrow(Resort), .6*nrow(Resort))
 train = Resort[trainInt,]
 test = Resort[-trainInt,]
 
 #Starting Model
-mlMod = randomForest(is_canceled ~ ., data = Resort, subset = trainInt)
+mlMod = randomForest(adr ~ ., data = Resort, subset = trainInt)
 mlMod
+#plotting error vs number of trees
+plot(mlMod)
 
-test.acc = double(10)
+test.err = double(10)
+
 #mtry is the number of variables randomly chosen at each split
 for(mtry in 1:10) 
 {
-  rf = randomForest(is_canceled ~ . , data = Resort, subset = trainInt, mtry = mtry,ntree = 400) 
-  prob.rf = predict(rf,test,type = "response") #Predictions on Test Set for each Tree
-  test.acc[mtry] = with(test, mean(is_canceled == prob.rf)) #Mean Squared Test Error
+  rf = randomForest(adr ~ . , data = Resort, subset = trainInt, mtry = mtry,ntree = 400) 
+  pred = predict(rf,test) #Predictions on Test Set for each Tree
+  test.err[mtry] = with(test, mean((adr - pred)^2)) #Mean Squared Test Error
 }
-test.acc
-#minmize MSE at mtry = 5
-max(test.acc)
+test.err
+#minmize MSE at mtry = 7
+min(test.err)
 
-mlMod2 = randomForest(is_canceled ~ . - children - babies + adults:children + adults:babies, data = Resort, subset = trainInt, mtry = 5,ntree = 400) 
+mlMod2 = randomForest(adr ~ . - children - babies + adults:children + adults:babies, data = Resort, subset = trainInt, mtry = 7,ntree = 400) 
 mlMod2
